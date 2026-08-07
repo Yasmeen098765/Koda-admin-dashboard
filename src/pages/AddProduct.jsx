af0c7b7395074e3c78668bdf6752bbc57292e2a1
yasmeen <nnanahamoud123@gmail.com>
2026-08-08 00:33:55 +0300

    tags

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -132,9 +132,9 @@ export default function AddProduct() {
       });
 
       //  إضافة tags
-      if (tags.length > 0) {
-        tags.forEach((tag) => data.append("tags", tag));
-      }
+     if (tags.length > 0) {
+      data.append("tags", JSON.stringify(tags));
+    }
 
       // إضافة الصور مباشرة (بدون تغيير الأسماء)
       images.forEach((image) => {
